30b3822da0160159f23b2ea3f88cdcc50a7ae9e5
Milad <khoshkaran.milad@gmail.com>
2021-07-28 12:21:30 +0200

    add about route

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get("/", (request, response) => {
     response.send("OK");
 });
 
+app.get("/about", (request, response) => {
+    response.send("OK");
+});
+
 app.listen(3000, () => console.log("Listening on port 3000"));
